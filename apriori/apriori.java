ce785203c02be357b7d21536e2f1166c7c9605a2
harsh911 <43216452+harsh911@users.noreply.github.com>
2019-09-26 04:25:32 +0530

    Update apriori.java

--- apriori/apriori.java
+++ apriori/apriori.java
@@ -2,78 +2,76 @@ package apriori;
 import java.util.*;
 import java.io.*;
 
-public class Apriori {
+public class apriori {
     public static void main(String []args) throws IOException{
         ArrayList<ArrayList<String>> list = new ArrayList<>();
         
         //System.out.print("Enter minimum support count : ");
-        int msc = 1;
+        int msc = 2;
+        double confidence = 0.5;
         //msc = new Scanner(System.in).nextInt();
       try{
-            FileReader fr = new FileReader("/home/student/NetBeansProjects/apriori/src/apriori/transactions.txt");
+             FileReader fr = new FileReader("C:\\Users\\Harshad\\Documents\\NetBeansProjects\\DataMining\\src\\apriori\\transactions.txt");
             BufferedReader br = new BufferedReader(fr);
             String s;
             ArrayList<String> tid = new ArrayList();
             ArrayList<String> next = new ArrayList();
             ArrayList<ArrayList<String>> nexts = new ArrayList();
+            Object assoc = null;
+            
             Map<ArrayList,Integer> items = new HashMap<>();
-
+            
             while((s = br.readLine()) != null){
                 StringTokenizer st = new StringTokenizer(s," ");
                 tid.add(st.nextToken());
                 ArrayList<String> tmp = new ArrayList(Arrays.asList(st.nextToken().split(",")));
                 Collections.sort(tmp);
-                  //System.out.println(tmp)  ; 
-                for(String obj:tmp){
-                    if(!next.contains(obj)){
-                        next.add(obj);
-                    }
-
-                }
+                
+                tmp.stream().filter(p ->!next.contains(p)).forEach(m -> next.add(m));
                 list.add(tmp);
             }
             System.out.println("Transactions");
             list.forEach(k -> System.out.println(k));
             int sz = next.size();
             Collections.sort(next);
-            System.out.println("\nDistinct items : "+next);
+            System.out.println("Distinct items : "+next);
 
             for(int n=2;!next.isEmpty();n++){
-                permutate(next,new LinkedList<Integer>(),items,sz-n+1,n,-1);
-            System.out.println("\n\n")  ;  
-            next.clear();
-            System.out.println(items);  
-            for(ArrayList k: items.keySet()){
-                for(ArrayList j: list){
-                    if(j.containsAll(k)){
-                        items.put(k, items.get(k)+1);
-                    }
+                items.clear();
+                nexts.clear();
+                
+                permute(next,new LinkedList<>(),items,next.size()-n+1,n,-1);
+                next.clear();
+                items.keySet().stream().forEach( k -> {
+                    list.stream().filter(j -> j.containsAll(k)).forEach( m -> items.put(k,items.get(k)+1));
+                }); 
+                items.keySet().stream().filter(b -> items.get(b) >=msc).forEach(b -> nexts.add(b));
+                
+                if(!nexts.isEmpty()){
+                    next.clear();
+                    nexts.stream().forEach( a -> {
+                        a.stream().filter(b->!next.contains(b)).forEach( c -> next.add(c));
+                    });
+                }
+                
+                if(!next.isEmpty()){
+                    System.out.println("Items : " +items);
+                    System.out.println("Candidate items: "+nexts);
+                    System.out.println("Next items : "+next); 
+                    assoc = nexts.clone();
                 }
             }
-            System.out.println("Items : " +items); 
-            for(ArrayList k: items.keySet()){
-                if(items.get(k) >= msc){
-                    nexts.add(k);
-                }
+            System.out.println("Next items : "+assoc+"  items: "+items); 
+            for(ArrayList l : (ArrayList<ArrayList<String>>)assoc){
+                System.out.println(l);
             }
-            System.out.println("Candidate items:");
-            nexts.forEach(k -> System.out.println(k));
-            for(ArrayList a : nexts){
-                for(Object b : a){
-                    if(!next.contains(b)) next.add(b.toString());
-                }
-            }
-            System.out.println("\nNext items : "+next); 
-            items.clear();
-            nexts.clear();
-            sz = next.size();
-            }
+ 
         }
         catch(IOException e){
             System.out.print("error :"+e);
         }
     }
-    public static void permutate(ArrayList next,LinkedList l,Map items,int size,int n,int it){
+    public static void permute(ArrayList next,LinkedList l,Map items,int size,int n,int it){
         for(int i=it+1;i<size;i++){ 
            l.add(i);
            if(n==1){
@@ -81,8 +79,7 @@ public class Apriori {
                 l.forEach(k -> alist.add(next.get((int)(k))));
                 items.put(alist,0);
             }
-           else
-            permutate(next,l,items,size+1,n-1,i);
+           else  permute(next,l,items,size+1,n-1,i);
            l.pollLast();
         } 
     }
